09ed5c5166061e879a71396950198314e142ad07
sr2005roy <sr2005roy@gmail.com>
2023-04-19 16:58:09 +0530

    Nav bar added to main page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./Header.css"
 
 export default function Header() {
   return (
-    <div>
-      Hello
-    </div>
+    <header>
+      <img src="src\assets\images\tp-removebg-preview.png" alt="myLogo" />
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Test</a></li>
+        <li><a href="#">Info</a></li>
+      </ul>
+    </header>
   )
 }
