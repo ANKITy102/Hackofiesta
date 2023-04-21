056d5e4f2368589e3c4f870db00b78f7c556b390
Ankit Yadav <119873198+ANKITy102@users.noreply.github.com>
2023-04-21 11:24:07 +0530

    flask-server

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,30 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Section from './components/Section'
+import Test from './components/Test'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [count, setCount] = useState(0)
   return (
+      <Router>
     <div>
       <Header />
-      <Section />
+      <Routes>
+       <Route exact path="/" element={<Section/>}/>
+       <Route exact path="/Test" element={<Test/>}/>
+       </Routes>
+   
+      
       <Footer />
     </div>
+    </Router>
   )
 }
 
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,8 +3,12 @@ import "./Section.css"
 
 export default function Section() {
   return (
-    <div className="section">
-      Hello World!!
+    <div className="main_section">
+      <div className="leftSection">
+        <span className="leftSection_head">PDetect</span>
+        <p>Our revolutionary voice-based Parkinson's disease detection system allows patients to take a simple test from the comfort of their own home. Using advanced machine learning algorithms, our system analyzes the patient's voice and extracts over 20 unique features to accurately diagnose Parkinson's disease. The test takes just a few minutes and the results are delivered instantly, providing patients with peace of mind and allowing them to seek medical attention sooner. Our system is non-invasive and requires no specialized equipment, making it an ideal tool for early Parkinson's detection in remote and underserved communities. Try our voice-based Parkinson's disease detection system today and take control of your health.</p>
+      </div>
+      <div className="rightSection"></div>
     </div>
   )
 }
